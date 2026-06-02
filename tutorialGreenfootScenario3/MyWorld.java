9bd33d0259345d7175f0751d23693efbfc775eac
BHASVIC-MiloBartlettBundy25 <milo.bartlettbundy25@students.bhasvic.ac.uk>
2026-06-02 14:56:36 +0100

    done tutorial 9

--- tutorialGreenfootScenario3/MyWorld.java
+++ tutorialGreenfootScenario3/MyWorld.java
@@ -18,7 +18,15 @@ public class MyWorld extends World
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1); 
         
-        addObject(new Animal(Greenfoot.getRandomNumber(360), Greenfoot.getRandomNumber(5)+ 1, 4),302,202);
+        
+    }
+    public void act()
+    {
+        if (Greenfoot.mouseClicked(null))
+        {
+            MouseInfo info = Greenfoot.getMouseInfo();
+            addObject(new Animal(Greenfoot.getRandomNumber(360), Greenfoot.getRandomNumber(5)+ 1, 4),info.getX(),info.getY());
+        }
     }
     /**
      * Prepare the world for the start of the program.
