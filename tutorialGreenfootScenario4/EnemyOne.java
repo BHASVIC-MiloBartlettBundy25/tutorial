205d338de83e33a55175a58f91d20d131efd1f33
BHASVIC-MiloBartlettBundy25 <milo.bartlettbundy25@students.bhasvic.ac.uk>
2026-06-03 11:45:04 +0100

    done tutorial 10

---
+++ tutorialGreenfootScenario4/EnemyOne.java
@@ -0,0 +1,24 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class EnemyOne here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class EnemyOne extends Enemy
+{
+    /**
+     * Act - do whatever the EnemyOne wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public EnemyOne()
+    {
+        setRotation(90);
+    }
+    public void act()
+    {
+        moveEnemy();
+    }
+    
+}
---
+++ tutorialGreenfootScenario4/MyWorld.java
@@ -0,0 +1,47 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class MyWorld here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class MyWorld extends World
+{
+
+    /**
+     * Constructor for objects of class MyWorld.
+     * 
+     */
+    public MyWorld()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(600, 600, 1); 
+        prepare();
+    }
+    public void act()
+    {
+        if (Greenfoot.getRandomNumber(60) < 1)
+        {
+            addEnemyOne();
+        }
+        
+    }
+    public void addEnemyOne()
+    {
+        addObject(new EnemyOne(), Greenfoot.getRandomNumber(600),0);
+    }
+    /**
+     * Prepare the world for the start of the program.
+     * That is: create the initial objects and add them to the world.
+     */
+    private void prepare()
+    {
+        Player player = new Player();
+        addObject(player,271,501);
+        EnemyOne enemyOne = new EnemyOne();
+        addObject(enemyOne,135,61);
+        EnemyTwo enemyTwo = new EnemyTwo();
+        addObject(enemyTwo,428,77);
+    }
+}
